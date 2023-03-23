fa0822807403db0c25357fadd1eed64beaecd835
firdoushsiddique62 <115089495+firdoushsiddique62@users.noreply.github.com>
2023-03-23 22:57:42 +0530

    Add files via upload

---
+++ NetBeansProjects/COMPRESSOR_DECOMPRESSOR/src/main/java/comp_decomp/compressor.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package comp_decomp;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ *
+ * @author Firdoush
+ */
+public class compressor {
+    
+    
+    public static void method(File file) throws IOException{
+        String filedirectory= file.getParent();
+        FileInputStream fis=new FileInputStream(file);
+        FileOutputStream fos=new FileOutputStream(filedirectory+"/Compressedfile.gz");
+        GZIPOutputStream gzipOS=new GZIPOutputStream(fos);
+        
+        byte[] buffer =new byte[1024];
+        int len;
+        
+        while((len=fis.read(buffer)) != -1){
+            gzipOS.write(buffer,0,len);
+        }
+        gzipOS.close();
+        fos.close();
+        fis.close();
+        
+    }
+    public static void main(String[] args) throws IOException{
+        File path=new File("C:\\Users\\cw\\Desktop\\compressorDecompressor\\hello.txt");
+        method(path);
+    }
+    
+    
+    
+}
